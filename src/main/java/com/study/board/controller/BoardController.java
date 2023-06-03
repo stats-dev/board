1c9c9a4f6fbbd618b4fcb7bdbc20fa321ff32412
stats-dev <hohun94@naver.com>
2023-06-03 17:04:35 +0900

    boardwrite and controller postmapping updates

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,7 +4,9 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -30,6 +32,49 @@ public class BoardController {
         boardService.write(board);
 
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+
+        model.addAttribute("list", boardService.boardList());
+
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view") // localhost:8080/board/view?id=1 이렇게 하면 1이 id 들어가서 게시글을 불러온다. parameter, get방식.
+    public String boardView(Model model, Integer id) {
+
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+        boardService.boardDelete(id);
+        //삭제 후 다시 목록(list) 페이지로 돌아간다.
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+
+        //기존에 있던 글 검색
+        Board boardTemp = boardService.boardView(id);
+        //수정시 넘어온 데이터 가져옴. 그래서 덮어씌운다.
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        return "redirect:/board/list";
     }
 
 }
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 // 이건 controller가 다시 사용한다.
 @Service
 public class BoardService {
@@ -12,8 +14,28 @@ public class BoardService {
     @Autowired //의존성 주입. 객체 생성자 필요없이 바로 주입한다.
     private BoardRepository boardRepository;
 
+
+    //글 작성
     public void write(Board board) {
         //board 변수를 넣어준다.
         boardRepository.save(board);
+    }
+
+
+    //게시글 리스트 처리
+    public List<Board> boardList() {
+        return boardRepository.findAll();
+    }
+
+    // 특정 게시글 불러오기
+    public Board boardView(Integer id) {
+
+        return boardRepository.findById(id).get();
+    }
+
+    // 특정 게시글 삭제
+    public void boardDelete(Integer id) {
+        // 게시글 번호 들어오면 Id 값에 해당하는 글을 삭제한다.
+        boardRepository.deleteById(id);
     }
 }
